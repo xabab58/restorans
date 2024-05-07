cbb545899f6f44182c4516a6cd75b9e6bce72a2a
xabab58 <xabab@yandex.ru>
2024-05-07 23:18:24 +0300

    доработка

--- restoran/posts/forms.py
+++ restoran/posts/forms.py
@@ -17,3 +17,19 @@ class AddScoreForm(forms.ModelForm):
     class Meta:
         model = Score
         fields = ['name', 'name_restoran', 'dish_name', 'rating']
+        labels = {
+            'name': 'Имя',
+            'name_restoran': 'Название ресторана',
+            'dish_name': 'Название блюда',
+            'rating': 'Рейтинг',
+        }
+    
+    def __init__(self, *args, **rwargs):
+        super(AddScoreForm, self).__init__(*args, **rwargs)
+
+        self.fields['name'].widget.attrs['class'] = 'form-control'
+        self.fields['name_restoran'].widget.attrs['class'] = 'form-control'
+        self.fields['dish_name'].widget.attrs['class'] = 'form-control'
+        self.fields['rating'].widget.attrs['class'] = 'form-control'
+
+        
