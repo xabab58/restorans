19cc00d708016b482fee07d3a5c48a0e879b024a
user <xabab@yandex.ru>
2024-05-08 10:49:19 +0300

    помогли на работе

--- restoran/posts/views.py
+++ restoran/posts/views.py
@@ -9,22 +9,25 @@ from .models import restorans
 from django.db import IntegrityError
 
 def index(request):
-    list = restorans.objects.all()
-
-    restaurant_ratings = Score.objects.values('name_restoran').annotate(avg_rating=Avg('rating'))
-    for restaurant in restaurant_ratings:
-        print(f"Ресторан: {restaurant['name_restoran']}, Средний рейтинг: {restaurant['avg_rating']}")
-
     restaurants_with_ratings = restorans.objects.annotate(avg_rating=Avg('score__rating'))
-
-    
+    reting=restorans.objects.annotate(avg_rating=Avg('score__rating')).order_by('-avg_rating').values('name')
+    # top_one=reting[0]
+    # top_two=reting[1]
+    # top_three=reting[2]
     for restaurant in restaurants_with_ratings:
         print(f"Ресторан: {restaurant.name}, Средний рейтинг: {restaurant.avg_rating}, Картинка: {restaurant.image.url if restaurant.image else 'Нет изображения'}")
 
+    rating_list = reting.values_list()
+    
+    names = [restaurant['name'] for restaurant in reting]
+    print('rating1111111111111111111111',names)
     context = {
-        'restorans': list,
-        'restaurant_ratings':restaurant_ratings,
-        'restaurants_with_ratings':restaurants_with_ratings
+        'restaurants_with_ratings':restaurants_with_ratings,
+        # 'top_one':top_one,
+        # 'top_two':top_two,
+        # 'top_three':top_three,
+        'reting':reting,
+        'names':names,
     }
 
     return render(request, 'posts/index.html', context)
